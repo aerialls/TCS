b889151554fa55e27a9857ae0403c55642de45f1
Julien Brochet <mewt@madalynn.eu>
2013-03-18 22:42:59 +0100

    added room schema

--- web/js/map.js
+++ web/js/map.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
-    var container = $('.map-container');
-    // var initialData = [false, false, true]; // display nothing
+    /* Tabs */
+    $('#tab-maps a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
 
     // Erase everything
     $('.layer').hide();
     $('.layer-checkbox').attr('checked', false);
-
-    /*
-    for (var i = 1; i <= initialData.length; i++) {
-        if (initialData[i - 1]) {
-
-        }
-    }
-    */
 
    $('.layer-checkbox').change(function() {
       var $this = $(this);
